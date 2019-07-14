8311751a8d19968f8db7ac5e48c81952d39ced73
Maxim <chernik.max@gmail.com>
2019-07-14 13:36:38 +0300

    [ADD] Finished the 1st page's component. Now it renders a download spinner on load and, after it, the list of employees with photos and some info.

---
+++ src/components/employeeCard/EmployeeCard.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './EmployeeCard.css';
+
+class EmployeeCard extends Component  {
+    render() { 
+        const {photo, id, firstName, lastName, position} = this.props;
+        return (
+            <div key={id} className="employee">
+                <img className="employee__photo" src={photo} alt=""></img>
+                <div className="employee__description">
+                    <span className="employee__text">Имя: {firstName}</span>
+                    <hr className="employee__line" />
+                    <span className="employee__text">Фамилия: {lastName}</span>
+                    <hr className="employee__line" />
+                    <span className="employee__text">Должность: {position}</span>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default EmployeeCard;
---
+++ src/containers/Employees.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { getEmployeesData } from '../redux/employeesList';
+import DownloadSpinner from '../components/downloadSpinner';
+import EmployeeCard from '../components/employeeCard/EmployeeCard'
+
+import './Employees.css';
+
+class List extends React.Component {
+
+  componentDidMount() {
+    const { employeesListRequest } = this.props;
+    employeesListRequest();
+  }
+
+  componentDidUpdate() {
+    console.log(this.props.employeesList);
+  }
+
+  render() {
+    const { employeesList: { isLoaded, data } } = this.props;
+
+    if (!isLoaded) return (
+      <div className="spinner-wrapper">
+        <DownloadSpinner />
+        <h2>Загрузка данных с сервера...</h2>
+      </div>
+    );
+
+    return (
+      <div className="employees">
+        {data.map(item => {
+          return <EmployeeCard
+            key={item.id}
+            photo={item.photo}
+            firstName={item.firstName}
+            lastName={item.lastName}
+            position={item.position}
+          />
+        })}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  employeesList: state.employeesList
+});
+
+const mapDispatchToProps = {
+  employeesListRequest: getEmployeesData,
+}
+export default connect(mapStateToProps, mapDispatchToProps)(List);
