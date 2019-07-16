da6b636b5ea620eb90aa205dd2a9bb2ef1144c8a
Maxim <chernik.max@gmail.com>
2019-07-16 03:58:58 +0300

    [ADD] Added slider with all employees to the top of employeeDetail's page, methods for it and two svg buttons for switching pages.

---
+++ src/components/icons/rightArrow.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const RightArrow = () => (
+    <svg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 199.404 199.404'>
+        <path fill='#6383a8' d='M 72.166382,200.23909 169.42597,98.153095 67.339973,0.89350861 39.748265,29.854428 112.88307,99.52207 43.205463,172.64738 Z'
+        />
+    </svg>
+);
--- src/containers/EmployeeDetails/EmployeeDetails.jsx
+++ src/containers/EmployeeDetails/EmployeeDetails.jsx
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getEmployeeData } from '../../redux/employeeDetails';
+import { getEmployeesData } from '../../redux/employeesList';
 import { postEmployeeComment } from '../../redux/employeeDetails';
 import { DownloadSpinner } from '../../components/downloadSpinner';
 import { Comments } from './Comments';
+import { EmployeeCard } from '../../components/employeeCard';
+import { Slider } from '../../components/Slider';
 import './EmployeeDetails.css';
 
 class EmployeeDetails extends Component {
     componentDidMount() {
-        console.log(this.props)
-        const { employeeDetailsRequest, match: { params: { employeeId } } } = this.props;
-        employeeDetailsRequest(employeeId);
-    }
+        const { 
+            employeeDetailsRequest, 
+            match: { params: { employeeId } },
+            employeesList,
+            employeesRequest
+        } = this.props;
 
-    componentDidUpdate() {
-        console.log(this.props.employeeInfo);
+        employeeDetailsRequest(employeeId);
+
+        if (!employeesList.isLoaded) {
+            employeesRequest();
+        }
+    }
+    
+    componentDidUpdate(prevProps) {
+        const { 
+            employeeDetailsRequest,
+            match: { params: { employeeId } } 
+        } = this.props;
+        if (employeeId !== prevProps.match.params.employeeId) {
+            employeeDetailsRequest(employeeId);
+        }
     }
 
     handleCommentAdding = (comment) => {
         const { postEmployeeComment, match: { params: { employeeId } } } = this.props;
         postEmployeeComment({ employeeId, ...comment });
+    }
+
+    getEmployeesCards() {
+        const {employeesList: {data}} = this.props;
+        
+        return data.map(item => {
+            return (
+                <EmployeeCard 
+                    key={item.id}
+                    employeeId={item.id}
+                    photo={item.photo}
+                    firstName={item.firstName}
+                    lastName={item.lastName}
+                    position={item.position}
+                    isShort={true}
+                />
+            );
+        })
     }
 
     render() {
@@ -30,12 +66,18 @@ class EmployeeDetails extends Component {
                 <DownloadSpinner />
                 <h2>Загрузка данных с сервера...</h2>
             </div>
-        );
-
-        console.log(this.handleCommentAdding)
+        );        
+        const {employeesList} = this.props;
 
         return (
             <div>
+                {(!employeesList.isLoaded) ?
+                    <span className="loading-message">Данные загружаются...</span>
+                    :
+                    <Slider 
+                        items={this.getEmployeesCards()}
+                    />
+                }
                 <div className="employee-details">
                     <img className="employee-details__photo" src={data.photo} alt=""></img>
                     <div className="employee-details__description">
@@ -61,12 +103,14 @@ class EmployeeDetails extends Component {
 }
 
 const mapStateToProps = state => ({
-    employeeInfo: state.employeeDetails
+    employeeInfo: state.employeeDetails,
+    employeesList: state.employeesList
 });
 
 const mapDispatchToProps = {
     employeeDetailsRequest: getEmployeeData,
     postEmployeeComment: postEmployeeComment,
+    employeesRequest: getEmployeesData,
 }
 
 
